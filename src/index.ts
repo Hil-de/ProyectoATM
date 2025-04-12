1bfcff704e1dff92bef1714763cabf3aed03459e
soft625 <soft6254467@gmail.com>
2025-04-12 12:46:44 -0400

    Version 1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import connectDB from './config/db';
+<<<<<<< HEAD
 
 // Rutas
 import clienteRoutes from './routes/cliente.route';
 import cuentaRoutes from './routes/cuenta.route';
 import tarjetaRoutes from './routes/tarjeta.route';
+=======
+>>>>>>> 4536f42 (Version 1)
 
 const app = express();
 const port = 3000;
 
+<<<<<<< HEAD
 // Middleware para leer JSON
 app.use(express.json());
 
@@ -16,6 +20,11 @@ app.use(express.json());
 connectDB();
 
 // Ruta raíz
+=======
+// Conectar a la base de datos
+connectDB();
+
+>>>>>>> 4536f42 (Version 1)
 app.get('/', (req, res) => {
   res.send('¡Hola Mundo!');
 });
