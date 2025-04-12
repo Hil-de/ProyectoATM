1ae764a78eb74f08ab46c4c978818b89978b635c
soft625 <soft6254467@gmail.com>
2025-04-12 12:46:44 -0400

    Version 1

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,16 @@
 import express from 'express';
 import connectDB from './config/db';
 
-// Rutas
-import clienteRoutes from './routes/cliente.route';
-import cuentaRoutes from './routes/cuenta.route';
-import tarjetaRoutes from './routes/tarjeta.route';
-
 const app = express();
 const port = 3000;
-
-// Middleware para leer JSON
-app.use(express.json());
 
 // Conectar a la base de datos
 connectDB();
 
-// Ruta raíz
 app.get('/', (req, res) => {
   res.send('¡Hola Mundo!');
 });
 
-// Rutas REST principales
-app.use('/clientes', clienteRoutes);
-app.use('/cuentas', cuentaRoutes);
-app.use('/tarjetas', tarjetaRoutes);
-
-// Escuchar en el puerto
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
