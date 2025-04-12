4536f425235118a8e90d05a883a1ac6e6333b5c0
soft625 <soft6254467@gmail.com>
2025-04-12 12:46:44 -0400

    Version 1

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-// src/index.ts
 import express from 'express';
+import connectDB from './config/db';
 
 const app = express();
 const port = 3000;
 
+// Conectar a la base de datos
+connectDB();
+
 app.get('/', (req, res) => {
-  res.send('¡Hola, Mundo!');
+  res.send('¡Hola Mundo!');
 });
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
