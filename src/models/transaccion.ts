a9bc563e2e5671672c5a0201cc2b2a9e6ff09568
soft625 <soft6254467@gmail.com>
2025-04-13 23:39:48 -0400

    version 1.9

--- src/models/transaccion.ts
+++ src/models/transaccion.ts
@@ -4,18 +4,16 @@ export interface ITransaccion extends Document {
   tipo: string;
   monto: number;
   fecha: Date;
-  resultado: string;
+  resultado: "exitoso" | "fallido";
   tarjetaId: Types.ObjectId;
-  atmId: Types.ObjectId;
 }
 
 const transaccionSchema = new Schema<ITransaccion>({
   tipo: { type: String, required: true },
   monto: { type: Number, required: true },
   fecha: { type: Date, default: Date.now },
-  resultado: { type: String, required: true },
+  resultado: { type: String, required: true, enum: ["exitoso", "fallido"] },
   tarjetaId: { type: Schema.Types.ObjectId, ref: 'Tarjeta', required: true },
-  atmId: { type: Schema.Types.ObjectId, ref: 'ATM', required: true }
 });
 
 export default model<ITransaccion>('Transaccion', transaccionSchema);
